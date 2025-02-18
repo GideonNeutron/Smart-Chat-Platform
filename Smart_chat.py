dce68398e89c960d77bad50cd8131d48ed117ab6
Gideon Appiah <gideonappiahl018@gmail.com>
2025-02-18 23:32:07 +0000

    Added Chainlit conversational memory

--- Smart_chat.py
+++ Smart_chat.py
@@ -2,25 +2,28 @@ from typing import List
 from ctransformers import AutoModelForCausalLM
 import chainlit as cl
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
-
 
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
-    print(f"Prompt created: {prompt}")
     return prompt
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    prompt = get_prompt(message.content)
-    response = llm(prompt)
-    await cl.Message(response).send()
+    message_history = cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
 
 @cl.on_chat_start
 def on_chat_start():
@@ -30,26 +33,6 @@ def on_chat_start():
     )
 
 
-"""
-history = [] 
 
-
-question = "Which city is the capital of India?"
-prompt = get_prompt(question)
-answer = ''
-
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-    answer += word
-print()
-history.append(answer)
-
-
-question = "And which is of the United States of America?"
-prompt = get_prompt(question, history)
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-print()
-"""
 
 print("DONE")
